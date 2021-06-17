bc93e86b2ffd811bba45ec826cd7653a0f742968
vagner domingues <domivabc@gmail.com>
2021-06-17 02:48:39 -0400

    Big Bang commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+    #skip_before_action :authenticate_user!, only: :home    
     before_action :authenticate_user!
+
+    def after_sign_in_path_for(resource)
+        events_path
+    end
 end
